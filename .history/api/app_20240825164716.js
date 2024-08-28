240c285747d2eda62cd77dec40a9b2d6fc574cc1
AiTrinh303 <trong.trinh@dci-student.org>
2024-08-28 23:49:28 +0200

    done with updatedProfile Page and update Prisma

---
+++ .history/api/app_20240825164716.js
@@ -0,0 +1,25 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import 'dotenv/config';
+import authRoute from "./routes/auth.route.js";
+// import userRoute from "./routes/user.route.js";
+// import postRoute from "./routes/post.route.js";k
+import check
+
+const PORT = process.env.PORT || 8000;
+const app = express();
+
+app.use(express.json());
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
+app.use(cookieParser());
+
+
+app.use("/api/auth", authRoute);
+// app.use("/api/users", userRoute);
+// app.use("/api/posts", postRoute);
+app.use("/api/check", checkroute);
+
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+  });
