da03b360f4b5ca33e96a2d9811c4616190078ea8
AiTrinh303 <trong.trinh@dci-student.org>
2024-10-13 21:42:31 +0200

    add .env into gitignore

--- .history/client/src/App_20241013213947.jsx
+++ .history/client/src/App_20241013213947.jsx
@@ -1,6 +1,6 @@
+import HomePage from "./routes/homePage/homePage";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import { Layout, RequireAuth } from "./routes/layout/layout";
-import HomePage from "./routes/homePage/homePage";
 import ListPage from "./routes/listPage/listPage";
 import SinglePage from "./routes/singlePage/singlePage";
 import ProfilePage from "./routes/profilePage/profilePage";
