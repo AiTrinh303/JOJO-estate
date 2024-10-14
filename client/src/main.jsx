d2f170d55246f7da43effa6e5c6ffabb1c54f511
AiTrinh303 <trong.trinh@dci-student.org>
2024-10-14 12:22:08 +0200

    add socket context

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,14 +3,14 @@ import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 import "./index.scss";
 import { AuthContextProvider } from "./context/AuthContext.jsx";
-// import { SocketContextProvider } from "./context/SocketContext.jsx";
+import { SocketContextProvider } from "./context/SocketContext.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <AuthContextProvider>
-      
+      <SocketContextProvider>
         <App />
-      
+      </SocketContextProvider>
     </AuthContextProvider>
   </React.StrictMode>
 );
