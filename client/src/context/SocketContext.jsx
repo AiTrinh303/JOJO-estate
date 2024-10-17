dbc35dc0e9c4d5cef8dfb086e132c46aed3955d5
AiTrinh303 <trong.trinh@dci-student.org>
2024-10-17 20:33:58 +0200

    fixed socket context

--- client/src/context/SocketContext.jsx
+++ client/src/context/SocketContext.jsx
@@ -9,7 +9,7 @@ export const SocketContextProvider = ({ children }) => {
   const [socket, setSocket] = useState(null);
 
   useEffect(() => {
-    setSocket(io("https://jojo-estate-socket.onrender.com" || "http://localhost:4444"));
+    setSocket(io("https://jojo-estate-socket.onrender.com"));
   }, []);
 
   useEffect(() => {
